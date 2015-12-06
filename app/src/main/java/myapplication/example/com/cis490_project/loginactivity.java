7cdafa200392014bfd0852f115244d20bbd15ad5
rafran07 <rafran07@louisville.edu>
2015-12-05 23:21:51 -0500

    Login information now goes into parse and is stored.

--- app/src/main/java/myapplication/example/com/cis490_project/loginactivity.java
+++ app/src/main/java/myapplication/example/com/cis490_project/loginactivity.java
@@ -1,38 +1,97 @@
 package myapplication.example.com.cis490_project;
 
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
-import android.content.pm.Signature;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Base64;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.parse.LogInCallback;
 import com.parse.Parse;
 import com.parse.ParseObject;
-
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
 
 public class loginactivity extends AppCompatActivity {
+
+    Button loginButton;
+   Button signUpButton;
+   EditText usernameField;
+    EditText passwordField;
+
+    String username;
+    String password;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loginactivity);
 
+        loginButton = (Button) findViewById(R.id.btlogin);
+        signUpButton = (Button) findViewById(R.id.btsignup);
+        usernameField = (EditText) findViewById(R.id.tName);
+        passwordField = (EditText) findViewById(R.id.tpassword);
+
+
         // Enable Local Datastore.
         Parse.enableLocalDatastore(this);
 
         Parse.initialize(this, "5JJQ74Au3x8kcf5GZySPXDNwVcwalXAH1VJ69unT", "TV7vSHWB0o4bXoZwWvgRNrNxoJtgCLsJtsvFfYrj");
 
-        ParseObject testObject = new ParseObject("TestSuper");
-        testObject.put("foo", "bar");
+        ParseObject testObject = new ParseObject("Testing");
+        testObject.put("foo", "badr");
         testObject.saveInBackground();
 
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                username = usernameField.getText().toString();
+                password = passwordField.getText().toString();
+                ParseUser.logInInBackground(username, password, new LogInCallback() {
+                    public void done(ParseUser user, com.parse.ParseException e) {
+                        if (user != null) {
+                            //start next activity
+                            //start sinch service
+                        } else {
+                        Toast.makeText(getApplicationContext(),
+                                "There was an error logging in.",
+                                Toast.LENGTH_LONG).show();
+                    }
+                }
+                });
+            }
+        });
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               username = usernameField.getText().toString();
+                  password = passwordField.getText().toString();
+                ParseUser user = new ParseUser();
+                user.setUsername(username);
+                user.setPassword(password);
+                user.signUpInBackground(new SignUpCallback() {
+                    public void done(com.parse.ParseException e) {
+                        if (e == null) {
+                            //start next activity
+                            //start sinch service
+                        } else {
+                            Toast.makeText(getApplicationContext(),
+                                    "There was an error signing up."
+                                    , Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+            }
+        });
     }
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
